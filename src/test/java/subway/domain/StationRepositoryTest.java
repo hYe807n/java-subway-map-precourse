b707fa9e91d284f2e240ff289874a15ce2fc9834
hYe807n <rlagkdus55@gmail.com>
2023-11-26 21:13:40 +0900

    fix: static 키워드로 인해서 StationRepository.java 객체 공유되는 문제 수정

--- src/test/java/subway/domain/StationRepositoryTest.java
+++ src/test/java/subway/domain/StationRepositoryTest.java
@@ -17,33 +17,33 @@ class StationRepositoryTest {
     @DisplayName("노선에 등록된 역 삭제 시 예외 처리")
     @Test
     void deleteStationByContainsLine() {
-        LineRepository.addSections("1호선", "1", "잠실역");
+        LineRepository.addSections("1호선", "1", "교대역");
         Assertions.assertThrows(IllegalArgumentException.class, () ->
-            StationRepository.deleteStation("잠실역"));
+            StationRepository.deleteStation("교대역"));
     }
 
     @DisplayName("역 삭제 기능")
     @Test
     void deleteStation() {
-        Station station = new Station("잠실역");
+        Station station = new Station("압구정역");
         StationRepository.addStation(station);
-        StationRepository.deleteStation("잠실역");
+        StationRepository.deleteStation("압구정역");
         Assertions.assertFalse(StationRepository.stations().contains(station));
     }
 
     @DisplayName("\"잠실역\" 역 검색하여 개체 반환하는 기능")
     @Test
     void searchStationByName() {
-        Station station = new Station("잠실역");
+        Station station = new Station("서울대입구역");
         StationRepository.addStation(station);
-        Assertions.assertEquals(StationRepository.searchStation("잠실역"), station);
+        Assertions.assertEquals(StationRepository.searchStation("서울대입구역"), station);
     }
 
     @DisplayName("\"잠실역\" 역 검색하여 없다면 null 반환하는 기능")
     @Test
     void searchStationByNameNull() {
-        Station station = new Station("잠실역");
+        Station station = new Station("잠실나루역");
         StationRepository.addStation(station);
-        Assertions.assertNull(StationRepository.searchStation("교대역"));
+        Assertions.assertNull(StationRepository.searchStation("강변역"));
     }
 }
