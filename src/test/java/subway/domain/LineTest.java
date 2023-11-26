f564f8b7c0293f482fdabac8ff466818f225b4e6
hYe807n <rlagkdus55@gmail.com>
2023-11-26 20:12:07 +0900

    test: 노선에 포함된 "잠실역" 구간 삭제하는 기능

--- src/test/java/subway/domain/LineTest.java
+++ src/test/java/subway/domain/LineTest.java
@@ -2,6 +2,7 @@ package subway.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,16 @@ class LineTest {
         line.addStations("잠실역");
         Assertions.assertThatIllegalArgumentException()
             .isThrownBy(() -> line.deleteSection(StationRepository.searchStation("잠실역")));
+    }
 
+    @DisplayName("노선에 포함된 구간 삭제")
+    @Test
+    void deleteSection() {
+        Line line = new Line("2호선");
+        line.addStations("잠실역");
+        line.addStations("강남역");
+        line.addStations("교대역");
+        line.deleteSection(StationRepository.searchStation("잠실역"));
+        assertThat(line.getStations().stream().map(Station::getName)).containsAll(Arrays.asList("강남역", "교대역"));
     }
 }
