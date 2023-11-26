e3cdecb2da55bd536c046e66e4b1a7f2a1916548
hYe807n <rlagkdus55@gmail.com>
2023-11-26 21:10:28 +0900

    fix: static 키워드로 인해서 LineRepository.java 객체 공유되는 문제 수정

--- src/test/java/subway/domain/LineRepositoryTest.java
+++ src/test/java/subway/domain/LineRepositoryTest.java
@@ -10,33 +10,33 @@ class LineRepositoryTest {
 
     @DisplayName("\"2호선\"을 노선에 추가")
     @Test
-    void addLines() {
+    void addLinesByName() {
         LineRepository.addLine(new Line("2호선"));
         Assertions.assertTrue(LineRepository.lines().stream().anyMatch( line ->
             line.getName().equals("2호선")));
     }
 
-    @DisplayName("\"2호선\" 노선 삭제")
+    @DisplayName("\"5호선\" 노선 삭제")
     @Test
     void deleteLineByName() {
-        LineRepository.addLine(new Line("2호선"));
-        LineRepository.deleteLineByName("2호선");
-        Assertions.assertNull(LineRepository.searchLine("2호선"));
+        LineRepository.addLine(new Line("5호선"));
+        LineRepository.deleteLineByName("5호선");
+        Assertions.assertNull(LineRepository.searchLine("5호선"));
     }
 
-    @DisplayName("노선 이름을 통해 \"2호선\" 노선 검색하는 기능")
+    @DisplayName("노선 이름을 통해 \"8호선\" 노선 검색하는 기능")
     @Test
     void searchLineByName() {
-        LineRepository.addLine(new Line("2호선"));
-        Assertions.assertEquals(LineRepository.searchLine("2호선").getName(), "2호선");
+        LineRepository.addLine(new Line("8호선"));
+        Assertions.assertEquals(LineRepository.searchLine("8호선").getName(), "8호선");
     }
 
-    @DisplayName("\"2호선\"에 \"신림역\" 구간 추가 기능")
+    @DisplayName("\"7호선\"에 \"신림역\" 구간 추가 기능")
     @Test
     void addSectionsByLineStation() {
-        LineRepository.addLine(new Line("2호선"));
-        LineRepository.addSections("2호선", "1", "신림역");
-        Assertions.assertEquals(LineRepository.searchLine("2호선").getStations()
+        LineRepository.addLine(new Line("7호선"));
+        LineRepository.addSections("7호선", "1", "신림역");
+        Assertions.assertEquals(LineRepository.searchLine("7호선").getStations()
                 .stream().map(Station::getName).collect(Collectors.toList()),
             Collections.singletonList("신림역"));
     }
