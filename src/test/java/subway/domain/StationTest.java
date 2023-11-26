064dbeeb6e58579fe05d6fbd900c42a7fb7a42db
hYe807n <rlagkdus55@gmail.com>
2023-11-26 21:15:15 +0900

    fix: static 키워드로 인해서 Station.java 객체 공유되는 문제 수정

--- src/test/java/subway/domain/StationTest.java
+++ src/test/java/subway/domain/StationTest.java
@@ -11,9 +11,9 @@ class StationTest {
     @DisplayName("역 이름 중복 시 예외 처리")
     @Test
     void addStationByDuplicateName() {
-        StationRepository.addStation(new Station("잠실역"));
+        StationRepository.addStation(new Station("구의역"));
         assertThrows(IllegalArgumentException.class, () ->
-            StationRepository.addStation(new Station("잠실역")));
+            StationRepository.addStation(new Station("구의역")));
     }
 
     @DisplayName("역 이름 2글자 이상이 아니라면 예외 처리")
@@ -26,14 +26,14 @@ class StationTest {
     @DisplayName("역이 노선에 포함되어 있다면 true 반환")
     @Test
     void stationContainsLine() {
-        LineRepository.addSections("2호선", "1", "신림역");
-        Assertions.assertTrue(StationRepository.searchStation("신림역").isLine());
+        LineRepository.addSections("2호선", "1", "합정역");
+        Assertions.assertTrue(StationRepository.searchStation("합정역").isLine());
     }
 
     @DisplayName("역이 노선에 포함되어 있지 않다면 false 반환")
     @Test
     void stationNotContainsLine() {
-        StationRepository.addStation(new Station("신림역"));
-        Assertions.assertFalse(StationRepository.searchStation("신림역").isLine());
+        StationRepository.addStation(new Station("이대역"));
+        Assertions.assertFalse(StationRepository.searchStation("이대역").isLine());
     }
 }
